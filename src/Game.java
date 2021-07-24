2488d3566fdb9f9c2186df9fe65f887320149ee2
chefmatty <mattioli.adam7@gmail.com>
2021-07-24 15:45:16 -0400

    Set up game loop and render method, draw rectangle to screen

--- src/Game.java
+++ src/Game.java
@@ -1,15 +1,90 @@
 import java.awt.Canvas;
+import java.awt.Graphics;
+import java.awt.Color;
+import java.awt.image.BufferStrategy;
 
 public class Game extends Canvas implements Runnable {
+
+    //private data
+    private boolean isRunning = false;
+    private Thread thread;
+
+    //the start method, which starts a thread
+    private void start(){
+        isRunning = true;
+        thread = new Thread(this);
+        thread.start();
+    }
+
+    //the stop method, which stops a thread
+    private void stop(){
+        isRunning = false;
+        try{
+            thread.join();
+        }
+        catch(InterruptedException e){
+            e.printStackTrace();
+        }
+    }
 
     //the constructor for the Game object
     public Game(){
         new Window(800, 800, "Survival Game", this);
+        start();
     }
 
-    //the run method
+    //the run method, which houses the game loop
     public void run(){
-        
+        this.requestFocus();
+        long lastTime = System.nanoTime();
+        double amountOfTicks = 60.0;
+        double ns = 1000000000 / amountOfTicks;
+        double delta = 0;
+        long timer = System.currentTimeMillis();
+        int frames = 0;
+
+        while(isRunning){
+            long now = System.nanoTime();
+            delta += (now - lastTime) / ns;
+            lastTime = now;
+            while(delta >= 1){
+                tick();
+                delta--;
+            }
+            render();
+            frames++;
+
+            if(System.currentTimeMillis() - timer > 1000){
+                timer += 1000;
+                frames = 0;
+            }
+        }
+        stop();
+    }
+
+    public void tick(){
+
+    }
+
+    public void render(){
+        //this block is pre-loading 2 frames in
+        BufferStrategy bs = this.getBufferStrategy();
+        if(bs == null){
+            this.createBufferStrategy(2);
+            return;
+        }
+
+        Graphics g = bs.getDrawGraphics();
+        ///////////////////////////
+        //Draw things below here!
+
+        g.setColor(Color.red);
+        g.fillRect(0, 0, 500, 500);
+
+        //Draw things above here!
+        ///////////////////////////
+        g.dispose();
+        bs.show();
     }
 
     //the main method
