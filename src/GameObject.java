ef0e194a5c53e903212a3b7e57ad8e218717a0af
Adam Mattioli <afm45@pitt.edu>
2021-07-24 17:15:00 -0400

    Finishing the GameObject abstract class

--- src/GameObject.java
+++ src/GameObject.java
@@ -1,4 +1,5 @@
 import java.awt.Graphics;
+import java.awt.Rectangle;
 
 public abstract class GameObject {
     
@@ -17,5 +18,40 @@ public abstract class GameObject {
     public abstract void tick();
     public abstract void render(Graphics g);
     public abstract Rectangle getBounds();
+
+    //getters and setters
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public float getVelX() {
+        return velX;
+    }
+
+    public void setVelX(float velX) {
+        this.velX = velX;
+    }
+
+    public float getVelY() {
+        return velY;
+    }
+
+    public void setVelY(float velY) {
+        this.velY = velY;
+    }
+
+    
 
 }
